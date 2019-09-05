c036ccb52e8d0fcfa6966438dce4f7d56026a4fa
miki31 <it.misha2016@gmail.com>
2019-09-05 16:57:10 +0300

    Start with VIEW on thymeleaf View all users

--- src/main/java/com/shop/olx_pets/controller/UserUiController.java
+++ src/main/java/com/shop/olx_pets/controller/UserUiController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +19,11 @@ public class UserUiController {
     @GetMapping
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/user/all")
+    public String homePage(Model model) {
+        model.addAttribute("users", userService.findAll());
+        return "users";
     }
 }
