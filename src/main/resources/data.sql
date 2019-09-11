75d6277b076d32822bb44880f5db7e224f8a47a1
miki31 <it.misha2016@gmail.com>
2019-09-11 23:03:08 +0300

    All testing users have password "123". first 5 users special create for testing change mistake with authority method --> hasAnyRole() change to hasAnyAuthority()

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,18 @@
 use olx_pets_db;
 
 INSERT INTO user (id, first_name, sur_name, nick_name, birthday, password, email, role) VALUES
-(1, 'Олег', 'Пашкевич', 'тічер', '1972-10-22', '$2a$10$.xysUEZxutiwXaduiJJ8oeWpXU9FbU5W7ow9.0KdjBepM0w86ZAT.', 'tich123@gmail.com', 'ADMIN'),
-(2, 'Мар''ян', 'Лукаш', 'NOтічер', '1988-02-11', 'NOtich123', 'NOtich123@gmail.com', 'GUEST' ),
-(3, 'Світлана', 'Федорчук', 'NOтічер_2', '1979-01-01', 'NOtich123NO', 'NOOOtich123@gmail.com', 'GUEST'),
-(4, 'Svitlana', 'Fedorchuk', 'Sveta', '1992-05-15', 'sveta', 'svetaF123@gmail.com', 'ADMIN'),
-(5, 'Misha', 'Shchepanskiy', 'mmm31', '1991-03-31', 'mmm31', 'mmm31@gmail.com', 'MANAGER' ),
-(6, 'Peter', 'Kolomya', 'Peterrr', '1966-06-09', 'petro', 'petre3@gmail.com', 'USER');
+(1, 'Admin', 'Sur_Admin', 'aaa', '1970-1-1', '$2a$10$.xysUEZxutiwXaduiJJ8oeWpXU9FbU5W7ow9.0KdjBepM0w86ZAT.', 'admin@gmail.com', 'ADMIN'),
+(2, 'Manager', 'Sur_Manager', 'mmm', '1975-1-1', '$2a$10$.xysUEZxutiwXaduiJJ8oeWpXU9FbU5W7ow9.0KdjBepM0w86ZAT.', 'manager@gmail.com', 'MANAGER'),
+(3, 'Seller', 'Sur_Seller', 'sss', '1980-1-1', '$2a$10$.xysUEZxutiwXaduiJJ8oeWpXU9FbU5W7ow9.0KdjBepM0w86ZAT.', 'seller@gmail.com', 'SELLER'),
+(4, 'User', 'Sur_User', 'uuu', '1985-1-1', '$2a$10$.xysUEZxutiwXaduiJJ8oeWpXU9FbU5W7ow9.0KdjBepM0w86ZAT.', 'user@gmail.com', 'USER'),
+(5, 'Guest', 'Sur_Guest', 'ggg', '1990-1-1', '$2a$10$.xysUEZxutiwXaduiJJ8oeWpXU9FbU5W7ow9.0KdjBepM0w86ZAT.', 'guest@gmail.com', 'GUEST'),
+
+(6, 'Олег', 'Пашкевич', 'тічер', '1972-10-22', '$2a$10$.xysUEZxutiwXaduiJJ8oeWpXU9FbU5W7ow9.0KdjBepM0w86ZAT.', 'tich123@gmail.com', 'ADMIN'),
+(7, 'Мар''ян', 'Лукаш', 'NOтічер', '1988-02-11', '$2a$10$.xysUEZxutiwXaduiJJ8oeWpXU9FbU5W7ow9.0KdjBepM0w86ZAT.', 'NOtich123@gmail.com', 'GUEST' ),
+(8, 'Світлана', 'Федорчук', 'NOтічер_2', '1979-01-01', '$2a$10$.xysUEZxutiwXaduiJJ8oeWpXU9FbU5W7ow9.0KdjBepM0w86ZAT.', 'NOOOtich123@gmail.com', 'GUEST'),
+(9, 'Svitlana', 'Fedorchuk', 'Sveta', '1992-05-15', '$2a$10$.xysUEZxutiwXaduiJJ8oeWpXU9FbU5W7ow9.0KdjBepM0w86ZAT.', 'svetaF123@gmail.com', 'ADMIN'),
+(10, 'Misha', 'Shchepanskiy', 'mmm31', '1991-03-31', '$2a$10$.xysUEZxutiwXaduiJJ8oeWpXU9FbU5W7ow9.0KdjBepM0w86ZAT.', 'mmm31@gmail.com', 'MANAGER' ),
+(11, 'Peter', 'Kolomya', 'Peterrr', '1966-06-09', '$2a$10$.xysUEZxutiwXaduiJJ8oeWpXU9FbU5W7ow9.0KdjBepM0w86ZAT.', 'petre3@gmail.com', 'USER');
 
 INSERT INTO category (id, name, description) VALUES
 (1, 'котик', 'коротко про милих котиків'),
