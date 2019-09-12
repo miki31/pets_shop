d5f6c65f753097474ac13d87919b0e8f10e25c04
fedorchuksveta <fedrchuksveta@gmail.com>
2019-09-12 16:20:16 +0300

    Created AdminController, adminHome.html

--- src/main/java/com/shop/olx_pets/controller/ui_controller/AdminController.java
+++ src/main/java/com/shop/olx_pets/controller/ui_controller/AdminController.java
@@ -1,4 +1,16 @@
 package com.shop.olx_pets.controller.ui_controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
+
+    @RequestMapping(value = {"/adminHome"}, method = RequestMethod.GET)
+    public String home() {
+        System.out.println("hello");
+        return "adminHome";
+    }
 }
