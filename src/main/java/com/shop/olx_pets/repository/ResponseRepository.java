3537a3ca371c25c0f29480ad90b22c48f0100c8f
miki31 <it.misha2016@gmail.com>
2019-10-12 01:44:37 +0300

    Create Repository and Service for Entity Response

---
+++ src/main/java/com/shop/olx_pets/repository/ResponseRepository.java
@@ -0,0 +1,15 @@
+package com.shop.olx_pets.repository;
+
+import com.shop.olx_pets.model.Response;
+import com.shop.olx_pets.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ResponseRepository extends JpaRepository<Response, Long> {
+
+    List<Response> findByReviewer(User reviewer);
+
+}
---
+++ src/main/java/com/shop/olx_pets/service/ResponseService.java
@@ -0,0 +1,22 @@
+package com.shop.olx_pets.service;
+
+import com.shop.olx_pets.model.Response;
+import com.shop.olx_pets.model.User;
+import com.shop.olx_pets.repository.ResponseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ResponseService {
+
+    @Autowired
+    ResponseRepository responseRepository;
+
+    public List<Response> findAllByReviewer(User reviewer) {
+        List<Response> responses = responseRepository.findByReviewer(reviewer);
+        return responses;
+    }
+
+}
