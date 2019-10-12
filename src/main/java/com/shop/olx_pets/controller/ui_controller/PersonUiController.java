5b45556ce690e5b8a1f7c17a6e8626f64c9f2a6c
miki31 <it.misha2016@gmail.com>
2019-10-13 02:03:58 +0300

    Button "View advertisements" show only for sellers. put button "Board" (view info page about oneself) in top navigation-bar

--- src/main/java/com/shop/olx_pets/controller/ui_controller/PersonUiController.java
+++ src/main/java/com/shop/olx_pets/controller/ui_controller/PersonUiController.java
@@ -7,12 +7,11 @@ import com.shop.olx_pets.service.ResponseService;
 import com.shop.olx_pets.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +29,11 @@ public class PersonUiController {
     @Autowired
     private ResponseService responseService;
 
+    @ModelAttribute("user")
+    public User activeUser(Authentication authentication) {
+        return userService.findUserByEmail(authentication.getName()).get();
+    }
+
     @GetMapping("info")
     public String getInfo(@RequestParam Long idUser, Model model) {
 //        model.addAttribute("id", id);
@@ -42,6 +46,25 @@ public class PersonUiController {
         model.addAttribute("response", response);
 
         List<Response> responses = responseService.findByAuthorIdAndSortByPosted(idUser);
+        model.addAttribute("responses", responses);
+
+//        Response response = new Response();
+//        model.addAttribute("response", response);
+
+        return "person/info_person";
+    }
+
+    @GetMapping("info/me")
+    public String getInfoMe(Model model) {
+
+        User user = activeUser(SecurityContextHolder.getContext().getAuthentication());
+        model.addAttribute("user", user);
+
+        Response response = new Response();
+        response.setGood(true);
+        model.addAttribute("response", response);
+
+        List<Response> responses = responseService.findByAuthorIdAndSortByPosted(user.getId());
         model.addAttribute("responses", responses);
 
 //        Response response = new Response();
